4ebafc9df957e7b78953f0a7988dde183ee01900
qcalmel <contact@quentincalmel.fr>
2022-01-20 09:55:59 +0100

    limit gameList api request to only main games (no dlc, special edition, etc...)

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -9,14 +9,18 @@ const getGameList = (query) => {
             'Client-ID': 'vdyvkae7tp66ret5xpmzl00h0je5eu',
             'Authorization': 'Bearer zacq1qom1jddqxijm9d5ioqb9hyo3m',
         },
-        data: `fields id,name,first_release_date,platforms.name; search "${query}"; limit 200;`
+        data:
+            `fields id,name,first_release_date,platforms.name;
+             search "${query}";
+             where version_parent = null & category = 0;
+             limit 200;`
     })
-        // .then(response => {
-        //     return response.data
-        // })
-        // .catch(err => {
-        //     console.error(err);
-        // });
+    // .then(response => {
+    //     return response.data
+    // })
+    // .catch(err => {
+    //     console.error(err);
+    // });
 }
 
 export {getGameList}
