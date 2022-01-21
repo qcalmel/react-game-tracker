dc29df92b4bb4d797cef5e1497b15d07181c3372
qcalmel <contact@quentincalmel.fr>
2022-01-21 10:14:37 +0100

    add function for get Games with tracked IDs

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -15,12 +15,23 @@ const getGameList = (query) => {
              where version_parent = null & category = 0;
              limit 200;`
     })
-    // .then(response => {
-    //     return response.data
-    // })
-    // .catch(err => {
-    //     console.error(err);
-    // });
 }
 
-export {getGameList}
+const getTrackedGameList = (trackedListID) => {
+
+    return axios({
+        url: `${process.env.REACT_APP_API}/games`,
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Client-ID': 'vdyvkae7tp66ret5xpmzl00h0je5eu',
+            'Authorization': 'Bearer zacq1qom1jddqxijm9d5ioqb9hyo3m',
+        },
+        data:
+            `fields id,name,first_release_date,platforms.name;
+             where id = (${trackedListID.toString()}) & version_parent = null & category = 0;
+             limit 200;`
+    })
+}
+
+export {getGameList,getTrackedGameList}
