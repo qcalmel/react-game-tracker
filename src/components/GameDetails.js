b872ef2b1587ce4e84f4eabe86b6e3670e6eb44a
qcalmel <contact@quentincalmel.fr>
2022-01-24 12:45:07 +0100

    add content to GameDetails component

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,10 +1,52 @@
 import * as React from "react";
-import { useNavigate } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {getSingleGame} from "../helpers/api";
 
-const GameDetails = ({gameID}) => {
-    const navigate = useNavigate();
+const GameDetails = ({gameID, isTracked, onTrack}) => {
+    const [game, setGame] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        getSingleGame(gameID)
+            .then(res => {
+                setGame(res.data[0])
+                setIsLoading(false)
+            })
+    })
     return (
-        <button onClick={() => navigate(-1)}>Go back</button>
+        <>
+            {
+                isLoading ?
+                    (
+                        <p>Loading...</p>
+                    )
+                    :
+                    (
+                        <div className='game-details'>
+                            <div className='game-cover'>
+                                <img
+                                    src={'//images.igdb.com/igdb/image/upload/t_cover_big/' + game.cover.image_id + '.jpg'}
+                                    alt='game cover'/>
+                                <input
+                                    onClick={() => onTrack(game.id)}
+                                    type='button'
+                                    value={isTracked ? "Untrack" : "Track"}
+                                    className={isTracked ? "btn btn-red" : "btn"}
+                                />
+                            </div>
+                            <div className='game-content'>
+                                <h3>{game.name}</h3>
+                                <p>Genre : {game.genres?.map((genre,i)=>(game.genres.length -1 === i ? genre.name :
+                                    genre.name + ", "))}</p>
+                                <p>Platforms : {game.platforms?.map((platform,i)=>(game.platforms.length -1 === i ? platform.name :
+                                    platform.name + ", "))}
+                                </p>
+                            </div>
+
+                        </div>
+
+                    )
+            }
+        </>
     )
 }
 
